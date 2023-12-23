ab25f98b5c3406cacead6e2c49b05d9091380ec7
Nha0127 <117083457+Nha0127@users.noreply.github.com>
2023-12-23 15:43:25 +0700

    ok

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
+const multer = require('multer');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -9,22 +10,35 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/upload')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+    }
+  })
 // Sử dụng multer để xử lý form-data
-const storage = multer.memoryStorage();
+//const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('textData'), (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
     // Truy cập dữ liệu text từ form-data thông qua req.file
     if (!req.file) {
       return res.status(400).send('No text data uploaded.');
     }
-  
+    if (req.file) {
+        req.body.image = req.file.originalname;
+    }
+
     // Xử lý dữ liệu text ở đây, ví dụ: lưu vào đĩa, xử lý, etc.
-    const textData = req.file.buffer.toString();
-    console.log('Received text:', textData);
-  
-    res.status(200).send('Text data uploaded successfully.');
+    // const textData = req.file.buffer.toString();
+    // console.log('Received text:', textData);
+    console.log(req.body)
+    res.status(200).send('Thanh cong');
   });
   
 app.post('/',(req,res)=>{
@@ -42,7 +56,7 @@ async function connectDB() {
         console.error(e);
     }
 }
-connectDB();
+//connectDB();
 app.listen("3000", () => {
     console.log("http://localhost:3000/");
 })
