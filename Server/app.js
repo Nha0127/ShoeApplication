87b64dd4e1f8ff6bfc686259cf4b9d789c4d7bba
Nha0127 <117083457+Nha0127@users.noreply.github.com>
2023-12-23 11:18:27 +0700

    ok

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,33 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Sử dụng multer để xử lý form-data
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('textData'), (req, res) => {
+    // Truy cập dữ liệu text từ form-data thông qua req.file
+    if (!req.file) {
+      return res.status(400).send('No text data uploaded.');
+    }
+  
+    // Xử lý dữ liệu text ở đây, ví dụ: lưu vào đĩa, xử lý, etc.
+    const textData = req.file.buffer.toString();
+    console.log('Received text:', textData);
+  
+    res.status(200).send('Text data uploaded successfully.');
+  });
+  
+app.post('/',(req,res)=>{
+    var name = req.body
+    console.log(name)
+    res.json("dsfdg")
+})
+
 async function connectDB() {
     const uri = "mongodb+srv://nha123:12345@atlascluster.rsxqmpm.mongodb.net/shoe?retryWrites=true&w=majority&appName=AtlasApp"
     try {
